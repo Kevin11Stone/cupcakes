0904582880f7821888c76a4cbe224c0f7782f2c2
Kevin Stone <ruca6616@gmail.com>
2023-01-05 19:24:09 -0500

    added comment header per assignment instruction

--- process.php
+++ process.php
@@ -1,3 +1,11 @@
+
+<!--
+Britany Torres, Kevin Stone
+01/05/2023
+URL: https://kevinstone.greenriverdev.com/328/cupcakes/
+Program description: Pair programming assignment practicing PHP form validation.
+-->
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,13 +19,12 @@
 
 <?php
 
-
-
 if(isset($_POST['flavor']) && !empty($_POST['name'])){
+    $userName = $_POST['name'];
     $flavorArray = $_POST['flavor'];
     $sum = 0;
 
-    echo "<h1>Thank you, " . $_POST['name'] .  ", for your order!</h1>";    echo "<h3>Order Summary: </h3>";
+    echo "<h1>Thank you, " . $userName .  ", for your order!</h1>";    echo "<h3>Order Summary: </h3>";
     foreach ( $flavorArray as $flavor ){
         $sum += 3.50;
         echo " - $flavor";
@@ -29,7 +36,6 @@ if(isset($_POST['flavor']) && !empty($_POST['name'])){
 else {
     echo "<h1>Please enter your name and choose a flavor</h1>";
 }
-
 
 ?>
 
